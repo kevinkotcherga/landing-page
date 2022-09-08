0e0620b3aaa130edc3974052f776d46834fd4732
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-09-08 13:52:18 +0200

    feat(home) : ajout des éléments de superposition

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../style/home.scss';
 import scroll from '../img/scroll.png';
+import spiral from '../img/spiral.png';
+import rectangle from '../img/rectangle.png';
 
 const Home = () => {
 	return (
 		<div className="home">
 			<h1 className="devInNet">Dev-in-net</h1>
 			<p className="description">NOUS DÉVELOPPONS VOS SOLUTIONS NUMERIQUES</p>
-			<img className="scroll" src={scroll} />
+			<img className="spiral" src={spiral} alt="" />
+			<img className="rectangle" src={rectangle} alt="" />
+			<img className="scroll" src={scroll} alt="" />
 		</div>
 	);
 };
