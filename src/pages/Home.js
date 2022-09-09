46f91c2ec31b9fe2a38eb4720f776203e0f7d88a
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-09-09 14:10:15 +0200

    ✨feat(home): ajout des textes de description de la société

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,18 +5,24 @@ import spiral from '../img/spiral.png';
 import rectangle from '../img/rectangle.png';
 
 const Home = () => {
-	window.addEventListener('scroll', () => {
-		const scrolled = window.scrollY;
 
-		console.log(scrolled);
-	});
+const [scrollBarPosition, setScrollBarPosition] = useState("");
+
+window.addEventListener("scroll", (event) => {
+  let scroll = window.scrollY;
+  setScrollBarPosition(scroll)
+});
+
+console.log(scrollBarPosition)
 
 	return (
 		<div id="home" className="home">
-			<h1 className="devInNet">Dev-in-net</h1>
-			<p className="description">NOUS DÉVELOPPONS VOS SOLUTIONS NUMERIQUES</p>
-			<img className="spiral" src={spiral} alt="" />
-			<img className="rectangle" src={rectangle} alt="" />
+			<h1 className="devinnet">Devinnet</h1>
+			<p className={(scrollBarPosition > 100) ? "description disappear-element" : "description appear-element"}>NOUS DÉVELOPPONS VOS SOLUTIONS NUMERIQUES</p>
+			<img className={(scrollBarPosition > 100) ? "spiral disappear-element" : "spiral appear-element"} src={spiral} alt="" />
+			<img className={(scrollBarPosition > 100) ? "rectangle disappear-element" : "rectangle appear-element"} src={rectangle} alt="" />
+      <p className={(scrollBarPosition > 100) ? "specialisation-frontend appear-element" : "specialisation-frontend disappear-element"}>Spécialisation Front-end</p>
+      <p className={(scrollBarPosition > 300) ? "integration appear-element" : "integration disappear-element"}>Intégration de qualité</p>
 			<img className="scroll" src={scroll} alt="" />
 		</div>
 	);
